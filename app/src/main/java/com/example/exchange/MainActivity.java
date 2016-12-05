f364944a2b7d14f42740c65593c11d59b0082bf6
wang <tttwttw@126.com>
2016-12-05 14:40:28 +0800

    can connect to jwxt now

--- app/src/main/java/com/example/exchange/MainActivity.java
+++ app/src/main/java/com/example/exchange/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.exchange;
 
+import android.app.DownloadManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Matrix;
@@ -15,6 +16,7 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -22,20 +24,27 @@ import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.security.MessageDigest;
+import java.util.ArrayList;
 import java.util.Map;
 import java.util.Set;
 
 public class MainActivity extends AppCompatActivity{
 
     private final static String checkcodeURL="http://uems.sysu.edu.cn/elect/login/code";
-    private final static String electURL="http://uems.sysu.edu.cn/elect/";
+    private final static String electURL="http://uems.sysu.edu.cn/elect";
+    private final static String loginURL="http://uems.sysu.edu.cn/elect/login";
     EditText username;
     EditText password;
     EditText checkcode;
     ImageView checkcodeImg;
     String mpassword;
-    String Cookie;
+    String sessionId="";
     Bitmap bitmap;
+    boolean connect_finish=false;
+   // HttpClient client;
+    //OkHttpClient client;
+
+
     private final static int CHECK_CODE=111;
     private final static int LOGIN_CODE=222;
     @Override
@@ -47,12 +56,16 @@ public class MainActivity extends AppCompatActivity{
         checkcode=(EditText)findViewById(R.id.checkcode);
         checkcodeImg=(ImageView)findViewById(R.id.img_checkcode);
         Button login=(Button)findViewById(R.id.login);
-        getCheckCode();
-        Log.i("miao","miao");
+        //client=new OkHttpClient();
+        //client=new DefaultHttpClient();
+        //getCheckCodeWithClient();
+        connectElectSystem();
+        //Log.i("miao","miao");
         checkcodeImg.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 getCheckCode();
+                //getCheckCodeWithClient();
             }
         });
         login.setOnClickListener(new View.OnClickListener() {
@@ -81,21 +94,90 @@ public class MainActivity extends AppCompatActivity{
                     e.printStackTrace();
                 }
                 loginElectSystem();
+                //loginWithClient();
                 //Toast.makeText(MainActivity.this, mpassword, Toast.LENGTH_SHORT).show();
             }
         });
 
     }
+/*
+    private void getCheckCodeWithClient(){
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try{
+                    Request request=new Request.Builder().url(checkcodeURL).build();
+                    Response response=client.newCall(request).execute();
+                    InputStream in=response.body().byteStream();
+                    bitmap= BitmapFactory.decodeStream(in);
+                    handler.sendEmptyMessage(CHECK_CODE);
+                    Log.i("response code",""+response.code());
+                    Headers header=response.headers();
+                    for(int i=0;i<header.size();i++){
+                        Log.d("Check "+header.name(i),header.value(i));
+                    }
+                    get_code_finish=true;
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+    }
 
+    private void loginWithClient(){
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    while (!get_code_finish);
+                    FormBody.Builder builder=new FormBody.Builder();
+                    builder.add("username", username.getText().toString())
+                            .add("password", mpassword)
+                            .add("j_code", checkcode.getText().toString())
+                            .add("lt", "")
+                            .add("_eventId", "submit")
+                            .add("gateway", "true");
+                    RequestBody formBody=builder.build();
+                    Request request=new Request.Builder()
+                            .url(loginURL)
+                            .post(formBody)
+                            .build();
+                    Response response=client.newCall(request).execute();
+                    Log.i("response code",""+response.code());
+                    Headers header=response.headers();
+                    for(int i=0;i<header.size();i++){
+                        Log.d("Check "+header.name(i),header.value(i));
+                    }
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+    }
+*/
     private void getCheckCode(){
         new Thread(new Runnable() {
             @Override
             public void run() {
                 try {
-                    HttpURLConnection connection=(HttpURLConnection)(new URL(checkcodeURL)).openConnection();
-                    Cookie=connection.getHeaderField("Set-Cookie");
-                    InputStream in=connection.getInputStream();
-                    showRequestHeader("check",connection);
+                    HttpURLConnection cnct=(HttpURLConnection)(new URL(checkcodeURL)).openConnection();
+                    if(sessionId.isEmpty()){
+                        String cookieValue=cnct.getHeaderField("Set-Cookie");
+                        sessionId=cookieValue.substring(0, cookieValue.indexOf(";"));
+                        cnct.setRequestProperty("Set-Cookie",sessionId);
+                    }else{
+                        cnct.addRequestProperty("Cookie",sessionId);
+                    }
+
+                    //String cookieValue=cnct.getHeaderField("Set-Cookie");
+                    //sessionId=cookieValue.substring(0, cookieValue.indexOf(";"));
+                    //cnct.setRequestProperty("Set-Cookie",Cookie);
+                    //Log.d("save cookie",sessionId);
+                    //cnct.addRequestProperty("Cookie",sessionId);
+                    //cnct.connect();
+
+                    InputStream in=cnct.getInputStream();
+                    showRequestHeader("check",cnct);
                     bitmap= BitmapFactory.decodeStream(in);
                     handler.sendEmptyMessage(CHECK_CODE);
                 }catch (Exception e){
@@ -105,18 +187,44 @@ public class MainActivity extends AppCompatActivity{
         }).start();
     }
 
+    private void connectElectSystem(){
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    HttpURLConnection cnct=(HttpURLConnection)(new URL(checkcodeURL)).openConnection();
+                    String cookieValue=cnct.getHeaderField("Set-Cookie");
+                    Log.d("Set-Cookie",cookieValue);
+                    sessionId=cookieValue.substring(0, cookieValue.indexOf(";"));
+                    Log.d("sessionId",sessionId);
+                    InputStream in=cnct.getInputStream();
+                    showRequestHeader("check",cnct);
+                    bitmap= BitmapFactory.decodeStream(in);
+                    handler.sendEmptyMessage(CHECK_CODE);
+
+                    HttpURLConnection connection;
+                    connection=(HttpURLConnection)(new URL(electURL)).openConnection();
+                    connection.addRequestProperty("Cookie",sessionId);
+                    showRequestHeader("connect",connection);
+
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
+
+            }
+        }).start();
+    }
     private void loginElectSystem(){
         new Thread(new Runnable() {
             @Override
             public void run() {
                 try{
-                    HttpURLConnection connection=(HttpURLConnection)(new URL(checkcodeURL)).openConnection();
                     StringBuilder sbR = new StringBuilder();
-
-                    connection.setDoInput(true);
-                    connection.setDoOutput(true);//允许连接提交信息
+                    Log.d("save cookie",sessionId);
+                    HttpURLConnection connection;
+                    connection=(HttpURLConnection)(new URL(loginURL)).openConnection();
+                    connection.addRequestProperty("Cookie",sessionId);
                     connection.setRequestMethod("POST");//网页默认“GET”提交方式
-
 
                     StringBuffer sb = new StringBuffer();
                     sb.append("username=").append(username.getText().toString());
@@ -131,10 +239,18 @@ public class MainActivity extends AppCompatActivity{
                     os.write(sb.toString().getBytes());
                     os.close();
 
-                    showRequestHeader("login",connection);
-                    //取Cookie
-                    BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                    String response=connection.getResponseMessage();
+                    Log.d("login response",response);
 
+                    showRequestHeader("login",connection);
+                    BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                    while (br.read()!=-1){
+                        String str=br.readLine();
+                        Log.d("body",str);
+                    }
+                    Log.d("url",connection.getURL().toString());
+                    //取Cookie
+//                    BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
 
                 }catch (Exception e){
                     e.printStackTrace();
